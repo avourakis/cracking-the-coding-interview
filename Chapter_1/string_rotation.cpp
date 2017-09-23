b5a12f606c7e0c2cd04c67b6239419eec267a22c
Andres Vourakis <avouraki@uci.edu>
2017-09-23 16:51:21 -0700

    Q1.9 skeleton

---
+++ Chapter_1/string_rotation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+
+/*
+ * String rotation: Assume you have a method isSubstring which check if one word is a substring of another. Given two strings, s1 and s2,
+ * write code to check if s2 is a rotation of s1 using only one call to isSubstring (e.g. "waterbottle" is a roation of "erbottlewat").
+ */
+
+bool is_substring()
+{
+
+
+    return true;
+}
+
+bool is_rotation()
+{
+
+    return true;
+}
+
+int main()
+{
+
+    return 0;
+}
