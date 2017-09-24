b9dfc90b45ff1e68d8f10d928a57e565cf484f4a
Andres Vourakis <avouraki@uci.edu>
2017-09-23 19:35:56 -0700

    Q1.8 not finished

--- Chapter_1/zero_matrix.cpp
+++ Chapter_1/zero_matrix.cpp
@@ -1,15 +1,61 @@
 #include <iostream>
 
-int m = 0;
-int n = 0;
-
-void zero_matrix(int mat[m][n])
+void zero_matrix(int ** matrix, int m, int n)
 {
-
+   for (int r = 0; r < m; ++r)
+   {
+       for(int c = 0; c < n; ++c)
+       {    
+            if(matrix[r][c] == 0)
+            {
+                // do something
+            }
+       }
+   } 
 }
 
 
 int main()
 {
+    int m = 3;
+    int n = 2;
+    
+    // Create an array of pointers that points to more arrays
+    // Assign 1s
+    int ** matrix;
+
+    matrix = new int*[m];
+    for (int r = 0; r < m; ++r)
+    {
+        matrix[r] = new int[n];
+        
+        for(int c = 0; c < n; ++c)
+            matrix[r][c] = 1;
+    }
+
+    // Assign one 0
+    matrix[1][0] = 0;
+
+    // Printing array
+
+    for (int r = 0; r < m; ++r)
+    {
+        for(int c = 0; c < n; ++c)
+        {
+            std::cout << matrix[r][c];
+        }
+        std::cout << "\n";
+    }
+
+    zero_matrix(matrix, m, n);
+
+    // Free each sub-array
+    for(int r = 0; r < m; ++r)
+        delete[] matrix[r];
+
+    // Free the array of pointers
+    delete[] matrix;
+
+
     return 0;
 }
